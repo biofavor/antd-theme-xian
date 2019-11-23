1fceb7e32f759c0823ed4d002b33ee954667a0eb
biofavor <biofavor@qq.com>
2019-11-23 13:50:54 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  '@primary-color': '#cc5004',
+  '@info-color': '#c73633',
+  '@highlight-color': '#c73633',
+  '@processing-color': '#c73633',
+  '@text-color': '#905c37',
+  '@layout-body-background': '#f4f3f1',
+  '@layout-header-background': '#152518',
+};
